36b9c65a7c6dde06e6a9efe83dd1b1c31e96933b
Nazariy <nazariy.holovach2007@gmail.com>
2025-02-28 11:52:43 +0200

    Fix a typo in the account models.py 'class Use' to 'class User'

--- backend/account/models.py
+++ backend/account/models.py
@@ -67,7 +67,7 @@ class CustomUserManager(UserManager):
         return self._create_user(name, email, password, **extra_fields)
     
 
-class Use(AbstractBaseUser, PermissionsMixin):
+class User(AbstractBaseUser, PermissionsMixin):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=100, blank=False)
