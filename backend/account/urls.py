8fb24163f9ef118828a0182a6d19f58ccb4bfd9a
Nazariy <nazariy.holovach2007@gmail.com>
2025-03-01 22:16:36 +0200

    Fixed an error in the signup URL pattern

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -4,7 +4,7 @@ from .apis import Signup
 
 
 urlpatterns = [
-    path('signup/', Signup.as_view(), 'signup'),
+    path('signup/', Signup.as_view(), name='signup'),
     path('login/', TokenObtainPairView.as_view(), name='token-obtain'),
     path('refresh/', TokenRefreshView.as_view(), name='token-refresh')
 ]
