9dc4f1dd28f8334f076f7f4bf2a9653e428af136
Nazariy <nazariy.holovach2007@gmail.com>
2025-03-01 19:43:06 +0200

    Added the login and refresh URL patterns

---
+++ backend/account/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .apis import Signup
+
+
+urlpatterns = [
+    path('signup/', Signup.as_view(), 'signup'),
+    path('login/', TokenObtainPairView.as_view(), name='token-obtain'),
+    path('refresh/', TokenRefreshView.as_view(), name='token-refresh')
+]
