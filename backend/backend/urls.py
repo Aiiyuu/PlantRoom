dcde29c083f12e0f91d3ca520c8321a82dbff180
Nazariy <nazariy.holovach2007@gmail.com>
2025-03-06 09:41:02 +0200

    Added the 'cart' URL pattern to the main URL pattern.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -22,7 +22,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/user/', include('account.urls')),
-    path('api/v1/inventory/', include('inventory.urls'))
+    path('api/v1/inventory/', include('inventory.urls')),
+    path('api/v1/cart/', include('cart.urls'))
 ]
 
 # This serves media files during development if DEBUG is True
