f14947b288d2ae46a1a90baedac6638af880fa08
Nazariy <nazariy.holovach2007@gmail.com>
2025-03-06 09:34:53 +0200

    Added the URL pattern for the plant detail API.

--- backend/inventory/urls.py
+++ backend/inventory/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .apis import PlantListAPI
+from .apis import PlantListAPI, PlantDetailAPI
 
 urlpatterns = [
     path('', PlantListAPI.as_view(), name='plant-list'),
+    path('plant/<uuid:id>/', PlantDetailAPI.as_view(), name='plant-detail')
 ]
 
