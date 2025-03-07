9195d9f04cce303867da1d7e9c669f5bab061e1a
Nazariy <nazariy.holovach2007@gmail.com>
2025-03-07 23:20:14 +0200

    Added the URL patterns for the IncreaseQuantityAPI and DecreaseQuantityAPI endpoints

--- backend/cart/urls.py
+++ backend/cart/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .apis import CartItemListAPI, AddCartItemAPI, DeleteCartItemAPI
+from .apis import CartItemListAPI, AddCartItemAPI, DeleteCartItemAPI, IncreaseQuantityAPI, DecreaseQuantityAPI
 
 urlpatterns = [
     path('', CartItemListAPI.as_view(), name='cart-items-list'),
     path('add/item/', AddCartItemAPI.as_view(), name='add-cart-item'),
-    path('remove/item/', DeleteCartItemAPI.as_view(), name='delete-cart-item'),
+    path('remove/item/<uuid:id>/', DeleteCartItemAPI.as_view(), name='delete-cart-item'),
+    path('item/increase-quantity/<uuid:id>/', IncreaseQuantityAPI.as_view(), name='increase-cart-item-quantity'),
+    path('item/decrease-quantity/<uuid:id>/', DecreaseQuantityAPI.as_view(), name='decrease-cart-item-quantity'),
 ]
