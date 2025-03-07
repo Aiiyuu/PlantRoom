9df553b9b2d709932007d801d9e9c33cf1d2d0a9
Nazariy <nazariy.holovach2007@gmail.com>
2025-03-07 13:27:09 +0200

    Added a new URL pattern for the deleting cart item.

--- backend/cart/urls.py
+++ backend/cart/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .apis import CartItemListAPI, AddCartItemAPI
+from .apis import CartItemListAPI, AddCartItemAPI, DeleteCartItemAPI
 
 urlpatterns = [
     path('', CartItemListAPI.as_view(), name='cart-items-list'),
-    path('add/item/', AddCartItemAPI.as_view(), name='add-cart-item')
+    path('add/item/', AddCartItemAPI.as_view(), name='add-cart-item'),
+    path('remove/item/', DeleteCartItemAPI.as_view(), name='delete-cart-item'),
 ]
