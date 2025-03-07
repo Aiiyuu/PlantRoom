0359fac1eafce0c10eb0fde5c1feb4041857844f
Nazariy <nazariy.holovach2007@gmail.com>
2025-03-07 10:38:06 +0200

    Added tests for the AddCartItemAPI endpoint.

---
+++ backend/cart/tests/test_apis.py
@@ -0,0 +1,279 @@
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.urls import reverse
+
+from inventory.tests.base_test import FileUploadTestCase
+
+from account.models import User
+from inventory.models import Plant
+from cart.models import Cart, CartItem
+
+
+class CartItemsListAPITest(FileUploadTestCase):
+    """
+    Test the functionality of the CartItemsListAPI endpoint.
+    
+    FileUploadTestCase is a custom base test case class that provides utility methods
+    to handle file uploads in Django tests. It automates the setup of a temporary
+    directory for storing uploaded files, ensuring that the `MEDIA_ROOT` is configured
+    properly during the test. It also ensures that files are cleaned up and deleted
+    after each test is run, preventing test pollution and improving isolation.
+    
+    Tests:
+        - Test access for authenticated users.
+        - Test access restrictions for unauthenticated users.
+        - Test the behavior when the cart is empty.
+        - Test the behavior when the Cart object does not exist.
+        - Test that the API endpoint successfully returns data.
+    """
+    
+    
+    def setUp(self):
+        
+        super().setUp()  # Call the setUp of FileUploadTestCase to handle media root setup
+        
+        # Initialize the APIClient instance for testing 
+        self.client = APIClient() # Create a new instance of the APIClient
+        self.url = reverse('cart-items-list') # Get the URL endpoint
+        
+        
+        # Create a Regular User object
+        self.user = User.objects.create_user(name='test_name', email='test@test.com', password='a12a14t56')
+        
+        # Create a few Plant objects
+        self.plant_obj1 = Plant.objects.create(name='Rosa', price=15.00, stock_count=28, image=self.create_valid_image())
+        self.plant_obj2 = Plant.objects.create(name='Violet', price=12.90, stock_count=50, image=self.create_valid_image())
+        self.plant_obj3 = Plant.objects.create(name='Chamomile', price=3.50, discount_percentage=20, image=self.create_valid_image())
+        
+        # Create a Cart object
+        self.cart = Cart.objects.create(user=self.user)
+        
+        # Create a few CartItems objects
+        self.cart_item_obj1 = CartItem.objects.create(cart=self.cart, product=self.plant_obj1)
+        self.cart_item_obj2 = CartItem.objects.create(cart=self.cart, product=self.plant_obj2)
+        self.cart_item_obj3 = CartItem.objects.create(cart=self.cart, product=self.plant_obj3)
+        
+    
+    def test_access_for_authenticated_user(self):
+        """Test whether an authenticated user can access the cart list API."""
+        
+        # Login user 
+        self.client.force_authenticate(user=self.user)
+        
+        # Send a GET request to the API endpoint
+        response = self.client.get(self.url)
+        
+        # Assert the status code is 200 (OK)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+        
+    def test_access_restriction_for_unauthenticated_user(self):
+        """Test that unauthenticated users are denied access to the API endpoint."""
+        
+        # Send a GET request to the API endpoint
+        response = self.client.get(self.url)
+        
+        # Assert the status code is 403 (Forbidden)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        
+        
+    def test_cart_is_empty(self):
+        """Test that the API endpoint returns a 200 status code when the cart items are not found."""
+        
+        # Make sure the cart is empty before the test
+        self.cart.cart_items.all().delete()  # Delete any CartItems if they exist
+
+        # Login user 
+        self.client.force_authenticate(user=self.user)
+
+        # Send a GET request to the API endpoint
+        response = self.client.get(self.url)
+
+        # Assert that the response status is 200 (OK) for empty cart
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Assert that the returned message is correct.
+        self.assertEqual(response.data['message'], 'Your cart is empty.')
+        
+    
+    def test_cart_object_not_found(self):
+        """
+        Ensure that the API endpoint returns a 404 status code when the Cart 
+        object does not exist.
+        """
+        
+        # Make sure that the Cart object is deleted before the test
+        self.cart.delete()
+        
+        # Login user
+        self.client.force_authenticate(user=self.user)
+        
+        # Send a GET request to the API endpoint.
+        response = self.client.get(self.url)
+        
+        # Assert the response status is 404 (Not Found)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        
+    
+    def test_cart_endpoint_returns_correct_data(self):
+        """Test that the API endpoint returns correct data."""
+        
+        # Login user
+        self.client.force_authenticate(user=self.user)
+        
+        # Send a GET request to the API endpoint
+        response = self.client.get(self.url)
+        
+        # Assert the status code is 200 (OK)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+        # Extract the response data
+        data = response.json()
+        
+        # Assert the structure of the response data
+        self.assertIn('items', data)
+        self.assertIn('total_cart_price', data)
+        self.assertIn('total_items_count', data)
+        
+        # Validate that 'items' contains the expected serialized data
+        self.assertEqual(data['total_cart_price'], float(self.cart.get_total())) # Test the total_cart_price method
+        self.assertEqual(data['total_items_count'], self.cart.get_items_count()) # Test the total_items_count method
+        
+        # Assert that the number of items matches the number of created objects.
+        self.assertEqual(len(data['items']), len(self.cart.cart_items.all()))
+        
+        # Ensure that the items include the plant field
+        self.assertIn('id', data['items'][0]['product'])
+        self.assertIn('name', data['items'][0]['product'])
+        self.assertIn('description', data['items'][0]['product'])
+        self.assertIn('price', data['items'][0]['product'])
+        self.assertIn('stock_count', data['items'][0]['product'])
+        self.assertIn('image', data['items'][0]['product'])
+        
+        
+    
+class AddCartItemAPITest(FileUploadTestCase):
+    """
+    Test case for verifying the functionalities of the API endpoint.
+    
+    FileUploadTestCase is a custom base test case class that provides utility methods
+    to handle file uploads in Django tests. It automates the setup of a temporary
+    directory for storing uploaded files, ensuring that the `MEDIA_ROOT` is configured
+    properly during the test. It also ensures that files are cleaned up and deleted
+    after each test is run, preventing test pollution and improving isolation.
+
+    Tests:
+        - Test access restriction for unauthenticated users.
+        - Test the behavior if the Cart object does not exist.
+        - Test the behavior it the Plant object does not exist.
+        - Test the behavior when the plant is already added to the cart.
+        - Test successful creation of the cart item.
+    """
+    
+    def setUp(self):
+        
+        super().setUp() # Call the setUp of FileUploadTestCase to handle media root setup
+        
+        # Initialize the APIClient instance for testing
+        self.client = APIClient() # Create a new instance of the APIClient
+        self.url = reverse('add-cart-item') # Get the URL endpoint
+        
+        # Create a regular User object
+        self.user = User.objects.create_user(name='test', email='test@test.com', password='a12a14t56')
+        
+        # Create a Cart object associated with the created User object.
+        self.cart = Cart.objects.create(user=self.user)
+        
+        # Create a Plant object
+        self.plant = Plant.objects.create(name='Rose', price=12.50, image=self.create_valid_image())
+        
+        # Create a CartItem object associated with the created Plant and Cart object.
+        self.cart_item = CartItem.objects.create(cart=self.cart, product=self.plant)
+        
+
+    def test_access_restriction_for_unauthenticated_user(self):
+        """Ensure that unauthenticated users cannot create a CartItem object."""
+        
+        # Make a POST request to the add-cart-item API endpoint
+        response = self.client.post(self.url, {'plant_id': str(self.plant.id)})
+        
+        # Assert the status_code is 403 (Forbidden)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+    def test_cart_object_not_found(self):
+        """
+        Ensure that the API endpoint correctly handles the case
+        when a Cart object is not found.
+        """
+
+        # Login user to avoid restrictions
+        self.client.force_authenticate(user=self.user)
+
+        # Make sure that the Cart object does not exist before
+        # making a POST request to the endpoint.
+        self.cart.delete() # Delete the object from the database
+
+        # Make a POST request to the add-cart-item API endpoint.
+        response = self.client.post(self.url, {'plant_id': str(self.plant.id)})
+
+        # Assert the status code is 404 (Not Found)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
+    def test_plant_object_not_found(self):
+        """
+        Ensure that the API endpoint correctly handles the case
+        when a Plant object is not found.
+        """
+
+        # Login user to avoid restrictions
+        self.client.force_authenticate(user=self.user)
+
+        # Get the Plant object id before deleting the object.
+        plant_id = self.plant.id
+
+        # Make sure that the Plant object does not exist before
+        # making a POST request to the endpoint.
+        self.plant.delete()
+
+        # Make a POST request to the add-cart-item API endpoint.
+        response = self.client.post(self.url, {'product_id': str(plant_id)})
+
+        # Assert the status code is 404 (Not Found)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
+    def test_plant_already_added_to_cart(self):
+        """
+        Ensure that the API endpoint correctly handles the case
+        when the Plant object is already added to the cart.
+        """
+
+        # Login user to avoid restriction
+        self.client.force_authenticate(user=self.user)
+
+        # Trying to create a CartItem that already exists
+        # Make a POST request to the add-cart-item API endpoint
+        response = self.client.post(self.url, {'product_id': str(self.plant.id)})
+
+        # Ensure that the CartItem object hasn't been created again.
+        self.assertEqual(self.cart.cart_items.all().count(), 1)
+
+
+    def test_successful_creation_of_cart_item(self):
+        """Test successful creation of a CartItem object."""
+
+        # Login user to avoid restrictions
+        self.client.force_authenticate(user=self.user)
+
+        # Create a new Plant object
+        new_plant = Plant.objects.create(name='violet', price=17.15, image=self.create_valid_image())
+
+        # Create a new CartItem object
+        new_cart_item = CartItem.objects.create(cart=self.cart, product=new_plant)
+
+        # Check that the CartItem object was created and associated with the Plant
+        self.assertIsInstance(new_cart_item, CartItem)
+        self.assertEqual(new_cart_item.product, new_plant)  # Ensure the CartItem is linked to the right plant
+
+
