aaf1467b96c5f9b20915c029477458d0fd2e5c5a
Nazariy <nazariy.holovach2007@gmail.com>
2025-03-09 20:46:04 +0200

    Created tests for the API endpoints, namely DeleteFeedbackAPI and CreateFeedbackAPI.

--- backend/feedback/tests/test_apis.py
+++ backend/feedback/tests/test_apis.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 from account.models import User
 from feedback.models import Feedback
 
+import uuid
 
 
 class FeedbackListAPITest(APITestCase):
@@ -134,3 +135,230 @@ class FeedbackListAPITest(APITestCase):
 
         # Assert the status code is 404 (Not Found)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
+
+class DeleteFeedbackAPITest(APITestCase):
+    """
+    Test case for verifying the functionalities of the DeleteFeedbackAPI endpoint.
+
+    Test:
+        - Test successful deletion.
+        - Test access restrictions for unauthenticated users.
+        - Test the case when the feedback object does not exist in the database.
+        - Test the case when the request.user is not the author of the Feedback object.
+    """
+
+    def setUp(self):
+        """Create test assets for the aforementioned tests."""
+
+        # Initialize the APIClient instance for testing
+        self.client = APIClient() # Create a new instance of the APIClient
+
+
+        # Create two User objects
+        self.author = User.objects.create_user(name='test', email='test@test.com', password='a12a14t56')
+        self.random_user = User.objects.create_user(name='test', email='test_@test.com', password='a12a14t56')
+
+        # Create a few Feedback objects
+        self.feedback1 = Feedback.objects.create(
+                                 user=self.author,
+                                 content='Great selection of products and fast delivery! Easy shopping experience.',
+                                 rating=3
+                                 )
+
+        self.feedback2 = Feedback.objects.create(
+                                 user=self.author,
+                                 content='Smooth checkout process, and my order arrived in perfect condition. Highly recommend!',
+                                 rating=2
+                                 )
+
+        # Define the URL path for the API endpoint
+        self.url = reverse('delete-feedback', kwargs={'id': self.feedback1.id})
+
+
+    def test_successful_deletion(self):
+        """Test successful deletion of the Feedback object."""
+
+        # Login user to avoid access restrictions
+        self.client.force_authenticate(user=self.author)
+
+        # Get the number of Feedback objects for the logged-in user.
+        number_of_feedbacks = self.author.feedbacks.all().count()
+
+        # Make a DELETE request to the delete-feedback API endpoint.
+        response = self.client.delete(self.url)
+
+        # Assert the status_code is 204 (No Content)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        # Assert that the Feedback object was deleted
+        self.assertEqual(self.author.feedbacks.all().count(), number_of_feedbacks - 1)
+
+
+    def test_access_restriction_for_unauthenticated_users(self):
+        """Make sure that unauthenticated users cannot access the delete-feedback API endpoint."""
+
+        # Get the number of Feedback objects in the database.
+        number_of_feedbacks = Feedback.objects.all().count()
+
+        # Make a DELETE request to the delete-feedback API endpoint.
+        response = self.client.delete(self.url)
+
+        # Assert the status_code is 403 (Forbidden)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        # Assert that the Feedback object wasn't deleted
+        self.assertEqual(Feedback.objects.all().count(), number_of_feedbacks)
+
+
+    def test_feedback_does_not_exist(self):
+        """Test case for verifying the behavior when no ID is provided."""
+
+        # Login user to avoid access restrictions
+        self.client.force_authenticate(user=self.author)
+
+        # Generate a random UUID not present in the database.
+        id = uuid.uuid4()
+
+        # Make a DELETE request to the delete-feedback API endpoint.
+        response = self.client.delete(reverse('delete-feedback', kwargs={'id': id}))
+
+        # Assert the status_code is 404 (Not Found)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
+    def test_user_is_not_author_of_feedback(self):
+        """
+        Test that the Feedback object is not deleted if the request user
+        is not the author of the Feedback object associated with the provided ID.
+        """
+
+        # Login user to avoid access restrictions
+        self.client.force_authenticate(user=self.random_user)
+
+        # Get the number of Feedback objects in the database
+        number_of_feedbacks = Feedback.objects.all().count()
+
+        # Make a DELETE request to the delete-feedback API endpoint.
+        response = self.client.delete(self.url)
+
+        # Assert the status_code is 403 (Forbidden)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        # Assert that the Feedback object wasn't deleted
+        self.assertEqual(Feedback.objects.all().count(), number_of_feedbacks)
+
+
+
+class CreateFeedbackAPI(APITestCase):
+    """
+    Test case for verifying the create-feedback API endpoint.
+
+    Tests:
+        - Test access restriction for unauthenticated users.
+        - Test the successful creation of a Feedback object.
+        - Test the case where a required field is missing.
+        - Test the behavior when invalid data is provided.
+    """
+
+    def setUp(self):
+
+        # Initialize the APIClient instance for testing
+        self.client = APIClient() # Create a new instance of the APIClient
+
+        # Retrieve the URL path to the create-feedback API endpoint.
+        self.url = reverse('create-feedback')
+
+        # Create a regular User object
+        self.user = User.objects.create_user(name='test', email='test@test.com', password='a12a14t56')
+
+
+    def test_access_restriction_for_unauthenticated_users(self):
+        """Ensure that unauthenticated users cannot access the API endpoint."""
+
+        # Create a data object with the required fields for the Feedback object
+        data = {
+            'content': 'Lorem ipsum dollar is amet.',
+            'rating': 5
+        }
+
+        # Make a POST request to the create-feedback API endpoint
+        response = self.client.post(self.url, data, format='json')
+
+        # Assert the status code is 403 (Forbidden)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+    def test_successful_feedback_creation(self):
+        """
+        Ensure that the create-feedback API endpoint returns
+        the created Feedback object with a 200 (OK) status code
+        after successfully handling the POST request.
+        """
+
+        # Login user to avoid access restriction
+        self.client.force_authenticate(user=self.user)
+
+        # Create a data object with the required fields for the Feedback object
+        data = {
+            'content': 'Lorem ipsum dollar is amet.',
+            'rating': 5
+        }
+
+        # Make a POST request to the create-feedback API endpoint
+        response = self.client.post(self.url, data, format='json')
+
+        # Assert the status code is 201 (Created)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # Assert that the created feedback contains the same field data.
+        self.assertEqual(response.data['content'], data['content'])
+        self.assertEqual(response.data['rating'], data['rating'])
+
+
+    def test_missing_required_field_for_feedback_creation(self):
+        """Test the behavior when a required field is missing."""
+
+        # Login user to avoid access restriction
+        self.client.force_authenticate(user=self.user)
+
+        # Create a data object with the required fields missing.
+        # Content field is not provided
+        data = {
+            'rating': 5
+        }
+
+        # Make a POST request to the create-feedback API endpoint
+        response = self.client.post(self.url, data, format='json')
+
+        # Assert the status code is 400 (Bad Request)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+    def test_invalid_data_is_provided(self):
+        """Test the behavior when invalid data is submitted."""
+
+        # Login user to avoid access restriction
+        self.client.force_authenticate(user=self.user)
+
+        # Create a data object with invalid field values.
+        data = {
+            'content': 'Too short!', # it must be between 20 and 800 characters long
+            'rating': 10 # It must be between 0 and 5 (inclusive)
+        }
+
+        # Make a POST request to the create-feedback API endpoint
+        response = self.client.post(self.url, data, format='json')
+
+        # Assert the status code is 400 (Bad Request)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        # Assert that the response contains error messages
+        self.assertIn('errors', response.data)
+
+        # Check for content validation error in the __all__ field
+        self.assertIn('The content field must include between 20 and 800 characters.', response.data['errors']['__all__'])
+
+        # Check for rating validation error in the __all__ field
+        self.assertIn('Constraint “rating_between_0_and_5” is violated.', response.data['errors']['__all__'])
