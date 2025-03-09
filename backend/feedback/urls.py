5f6d752728b19fb7126e2f976242b9834d4520d0
Nazariy <nazariy.holovach2007@gmail.com>
2025-03-09 20:27:12 +0200

    Added the URL path to the create-feedback API endpoint.

--- backend/feedback/urls.py
+++ backend/feedback/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .apis import FeedbackListAPI
+from .apis import FeedbackListAPI, DeleteFeedbackAPI, CreateFeedbackAPI
 
 urlpatterns = [
     path('', FeedbackListAPI.as_view(), name='feedback-list'),
+    path('delete/<uuid:id>/', DeleteFeedbackAPI.as_view(), name='delete-feedback'),
+    path('create/', CreateFeedbackAPI.as_view(), name='create-feedback')
 ]
