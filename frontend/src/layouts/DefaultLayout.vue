c01ab6fac346f198ab25d8f51b2f2562db383252
Nazariy <nazariy.holovach2007@gmail.com>
2025-06-22 00:53:29 +0300

    Added the base layout

---
+++ frontend/src/layouts/DefaultLayout.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="default-layout">
+        <AppNavbar />
+
+        <main>
+            <slot />
+        </main>
+
+        <AppFooter />
+    </div>
+</template>
+
+<script setup lang="ts">
+import AppNavbar from "@/components/ui/AppNavbar.vue";
+import AppFooter from "@/components/ui/AppFooter.vue";
+</script>
