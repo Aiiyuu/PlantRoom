da27d4c0e7430cc12be78133b6abe685ddf67bb8
Nazariy <nazariy.holovach2007@gmail.com>
2025-06-23 01:25:54 +0300

    Update max-width of default layout

--- frontend/src/layouts/DefaultLayout.vue
+++ frontend/src/layouts/DefaultLayout.vue
@@ -2,11 +2,11 @@
     <div class="default-layout font-outfit">
         <AppNavbar />
 
-        <main class="max-w-[1728px] mx-auto">
+        <main class="max-w-[1400px] mx-auto">
             <slot />
         </main>
 
-        <AppFooter class="max-w-[1728px] mx-auto" />
+        <AppFooter class="max-w-[1400px] mx-auto" />
     </div>
 </template>
 
