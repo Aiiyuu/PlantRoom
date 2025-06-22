8773ab23d08d22c1df0302f58a0e870721368649
Nazariy <nazariy.holovach2007@gmail.com>
2025-06-22 18:49:07 +0300

    Added image base URL

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -7,12 +7,21 @@ import axios from 'axios'
 // Import styles
 import './assets/styles/tailwind.css'
 
+// Set the default baseURL for all Axios requests
+axios.defaults.baseURL = 'http://127.0.0.1:8000/api/v1/'
+
 const app = createApp(App)
+
+// Set the global image base URL
+app.config.globalProperties.$imageBaseUrl = 'http://127.0.0.1:8000/'
 
 // Create Pinia instance
 const pinia = createPinia()
 
 app.use(pinia)    // <-- register Pinia
 app.use(router)   // <-- register router
+
+// Inject the $imageBaseUrl globally
+app.provide('$imageBaseUrl', 'http://127.0.0.1:8000/')
 
 app.mount('#app')
