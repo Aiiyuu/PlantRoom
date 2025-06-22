30b2d38a053f6859e658acd208fbaeefc6bd04c6
Nazariy <nazariy.holovach2007@gmail.com>
2025-06-22 03:10:43 +0300

    Added spinner component

---
+++ frontend/src/components/ui/LoadingSpinner.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="spinner"></div>
+</template>
+
+<style>
+.spinner {
+    @apply w-16 h-16 border-solid border-8 border-peach-orange border-r-cream rounded-full;
+    animation: spinner 1s linear infinite;
+}
+
+@keyframes spinner {
+    from {
+        transform: rotate(0);
+    }
+    to {
+        transform: rotate(360deg);
+    }
+}
+</style>
