96f347e268c4bc909eb9e3fa1a834113db77c258
Nazariy <nazariy.holovach2007@gmail.com>
2025-06-22 17:43:05 +0300

    Add trending products component

---
+++ frontend/src/pages/Home/TrendingProductsBlock.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="trending-products"></div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped>
+.trending-products {
+    @apply w-full min-h-screen bg-cream;
+}
+</style>
