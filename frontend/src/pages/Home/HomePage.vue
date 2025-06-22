1a28286ffd26806f706648fe5245b3b5ebcf4c12
Nazariy <nazariy.holovach2007@gmail.com>
2025-06-22 13:46:59 +0300

    Includ about us component

--- frontend/src/pages/Home/HomePage.vue
+++ frontend/src/pages/Home/HomePage.vue
@@ -2,6 +2,8 @@
     <div>
         <HeaderBlock />
 
+        <AboutUsBlock />
+
         <LocationMap />
     </div>
 </template>
@@ -9,4 +11,5 @@
 <script lang="ts" setup>
 import LocationMap from "@/pages/Home/LocationMap.vue"
 import HeaderBlock from "@/pages/Home/HeaderBlock.vue"
+import AboutUsBlock from "@/pages/Home/AboutUsBlock.vue"
 </script>
