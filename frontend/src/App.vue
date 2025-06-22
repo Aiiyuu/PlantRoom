c82bd31d3ce7f0b4c86ad3755c24bc0c871c9256
Nazariy <nazariy.holovach2007@gmail.com>
2025-06-22 03:12:34 +0300

    Remove unnecessary tags

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,25 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <DefaultLayout>
+        <router-view/>
+    </DefaultLayout>
 </template>
 
+<script lang="ts" setup>
+import DefaultLayout from "@/layouts/DefaultLayout.vue";
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+    @apply font-outfit;
 }
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+//nav {
+//  a {
+//    font-weight: bold;
+//    color: #2c3e50;
+//
+//    &.router-link-exact-active {
+//      color: #42b983;
+//    }
+//  }
+//}
 </style>
