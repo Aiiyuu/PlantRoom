6bf33418fe2f2bc0f8f24ac3c4c358f73c4de495
Nazariy <nazariy.holovach2007@gmail.com>
2025-06-22 13:02:55 +0300

    Includ header component

--- frontend/src/pages/Home/HomePage.vue
+++ frontend/src/pages/Home/HomePage.vue
@@ -1,9 +1,12 @@
 <template>
     <div>
+        <HeaderBlock />
+
         <LocationMap />
     </div>
 </template>
 
 <script lang="ts" setup>
 import LocationMap from "@/pages/Home/LocationMap.vue"
+import HeaderBlock from "@/pages/Home/HeaderBlock.vue"
 </script>
