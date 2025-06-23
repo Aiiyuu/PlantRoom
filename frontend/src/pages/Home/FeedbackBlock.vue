27c62e38b6e46c0c30eb36a07babbe203c519f75
Nazariy <nazariy.holovach2007@gmail.com>
2025-06-23 19:36:42 +0300

    Create feedback component

---
+++ frontend/src/pages/Home/FeedbackBlock.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="feedback" id="feedbacks">
+        <h1 class="feedback__title">
+            Our happy customers
+        </h1>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import Feedback from '@/types/FeedbackInterface'
+
+</script>
+
+
+<style lang="scss" scoped>
+.feedback {
+    @apply relative w-full py-8 px-8 md:px-16 bg-cream;
+
+    &__title {
+        @apply text-4xl font-medium;
+    }
+}
+</style>
