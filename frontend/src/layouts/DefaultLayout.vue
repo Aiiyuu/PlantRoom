d15998021ac8ce8f1dbf5c2da9cfc1032ec8e21d
Nazariy <nazariy.holovach2007@gmail.com>
2025-06-26 20:24:08 +0300

    Move max width into a dedicated variable

--- frontend/src/layouts/DefaultLayout.vue
+++ frontend/src/layouts/DefaultLayout.vue
@@ -2,11 +2,11 @@
     <div class="default-layout font-outfit">
         <AppNavbar />
 
-        <main class="max-w-[1728px] mx-auto">
+        <main class="max-w-screen-w mx-auto">
             <slot />
         </main>
 
-        <AppFooter class="max-w-[1728px] mx-auto" />
+        <AppFooter class="max-w-screen-w mx-auto" />
     </div>
 </template>
 
