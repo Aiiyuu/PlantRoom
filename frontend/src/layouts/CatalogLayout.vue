e8614be5912eb7e1964365e9f8dfd5ba8a0c4617
Nazariy <nazariy.holovach2007@gmail.com>
2025-06-27 01:50:11 +0300

    Update catalog layout

--- frontend/src/layouts/CatalogLayout.vue
+++ frontend/src/layouts/CatalogLayout.vue
@@ -2,10 +2,10 @@
     <div class="font-outfit">
         <AppNavbar />
 
-        <div class="main-content max-w-screen-w">
+        <div class="main-content max-w-screen-w mx-auto">
             <FilterBar class="filter-bar" />
 
-            <main>
+            <main class="w-full">
                 <slot />
             </main>
         </div>
@@ -23,11 +23,16 @@ import FilterBar from "@/pages/Catalog/FilterBar.vue"
 
 <style lang="scss" scoped>
 .main-content {
-    @apply w-full flex justify-between px-8 md:px-16 py-16;
+    @apply w-full flex flex-col lg:flex-row justify-between px-8 md:px-16 py-16;
 
     .filter-bar {
-        @apply sticky top-12 left-0 h-full;
-        height: calc(100vh - 3rem * 2);
+        @apply top-12 left-0 h-full w-full mb-10 lg:sticky lg:w-[350px] lg:mb-0;
+    }
+}
+
+@media (min-width: 1024px) {
+    .filter-bar {
+        height: calc(100vh - 3rem * 2) !important;
     }
 }
 </style>
