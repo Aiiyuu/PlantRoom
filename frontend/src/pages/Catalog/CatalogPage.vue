d3fb5d971fd086fb340b85bd7fe4bc8c2f833ea7
Nazariy <nazariy.holovach2007@gmail.com>
2025-06-26 19:37:33 +0300

    Create catalog and filter bar components

---
+++ frontend/src/pages/Catalog/CatalogPage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="catalog">
+        <FilterBar />
+
+
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { useInventoryStore } from "@/stores/inventory"
+import Plant from '@/types/PlantInterface'
+import FilterBar from "@/pages/Catalog/FilterBar.vue"
+</script>
+
+<style lang="scss" scoped>
+    .catalog {
+        @apply w-full min-h-screen p-8 md:px-16 flex justify-between;
+    }
+</style>
